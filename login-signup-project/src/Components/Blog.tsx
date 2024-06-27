f8293bb92788721a7e28630701a13ac5ede50324
mfaiz-ahmed <faizmaqsood668@gmail.com>
2024-06-27 16:34:17 +0500

    login project

--- login-signup-project/src/Components/Blog.tsx
+++ login-signup-project/src/Components/Blog.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import Post1 from './Assets/post-item1.jpg'
 import Post2 from './Assets/post-item2.jpg'
 import Post3 from './Assets/post-item3.jpg'
+import Insta1 from './Assets/insta-item1.jpg'
+import Insta2 from './Assets/insta-item2.jpg'
+import Insta3 from './Assets/insta-item3.jpg'
+import Insta4 from './Assets/insta-item4.jpg'
+import Insta5 from './Assets/insta-item5.jpg'
+
+
 
 export default function Blog() {
   return (
     <>
+    <div>
      <h1 id='blog' className='heading'>LATEST POSTS</h1> 
     <div className='d-flex justify-content-center'>
     <div>
@@ -30,6 +38,17 @@ export default function Blog() {
         </div>
     </div>
     </div>
-    </>
+    <div className='container'>
+      <h1 className='heading'>SHOP OUR INSTA</h1>
+      <div className='text-center'>
+        <img className='insta-img m-3' src={Insta1} alt="Insta pics" />
+        <img className='insta-img m-3' src={Insta2} alt="Insta pics" />
+        <img className='insta-img m-3' src={Insta3} alt="Insta pics" />
+        <img className='insta-img m-3' src={Insta4} alt="Insta pics" />
+        <img className='insta-img m-3' src={Insta5} alt="Insta pics" />
+      </div>
+    </div>
+</div>
+  </>
   )
 }
