aab3ff0dc08411864fb7eaf940a854b9677a5779
Faiz Ahmed <faizmaqsood668@gmail.com>
2024-06-28 07:36:49 -0700

    products added

--- login-signup-project/src/Components/Products.tsx
+++ login-signup-project/src/Components/Products.tsx
@@ -1,160 +1,93 @@
 import React from 'react'
-import Mobile1 from './Assets/Mobile-1.webp'
+import Product1 from './Assets/Product-1.webp'
+import Product2 from './Assets/Product-2.webp'
+import Product3 from './Assets/Product-3.webp'
+import Product4 from './Assets/Product-4.png'
+import Product5 from './Assets/Product-5.webp'
+import Product6 from './Assets/Product-6.webp'
+import Product7 from './Assets/Product-7.webp'
+import Product8 from './Assets/Product-8.webp'
+import Product9 from './Assets/Product-9.webp'
+import Product10 from './Assets/Product-10.webp'
+import Product11 from './Assets/Product-11.webp'
+import Product12 from './Assets/Product-12.webp'
+
+
 
 export default function Products() {
     const phones = [
         {
-            image: "./Assets/Mobile-1.webp",
+            image: './Assets/Product-1.webp',
             brand: 'Apple',
             model: 'Iphone 15 Pro Max',
-            ram: "8 GB",
-            rom: "256 GB",
-            maincamera: '48 MP + 12 MP + 12 MP',
-            frontCamera: '12 MP',
             price: 525000
         },
         {
-            image: "./Assets/Mobile-2.webp",
-            brand: 'Samsung',
-            model: 'S24 Ultra',
-            ram: "12 GB",
-            rom: "512 GB",
-            maincamera: "200 MP + 50 MP + 10 MP",
-            frontCamera: '12 MP',
-            price: 475000
+            image: './Assets/Product-2.webp',
+            brand: 'Sony',
+            model: 'Playstation 4 Slim',
+            price: 61500
         },
         {
-            image: "./Assets/Mobile-3.webp",
+            image: './Assets/Product-3.webp',
             brand: 'Samsung',
+            model: '43 inch LED Smart TV',
+            price: 136900
+        },
+        {
+            image: './Assets/Product-4.png',
+            brand: 'Zero Lifestyle',
+            model: 'Ignite Smart Watch',
+            price: 7000
+        },
+        {
+            image: './Assets/Product-5.webp',
+            brand: 'Lenovo',
+            model: 'Livepods XT88',
+            price: 3000
+        },
+        {
+            image: './Assets/Product-6.webp',
+            brand: 'Samsung Galaxy',
             model: 'A15',
-            ram: "3 GB",
-            rom: "32 GB",
-            maincamera: '50 MP + 5 MP + 2 MP',
-            frontCamera: '13 MP',
-            price: 12500
+            price: 49500
         },
         {
-            image: "./Assets/Mobile-4.jpg",
-            brand: 'Oneplus',
-            model: 'Oneplus 10 Pro',
-            ram: "12 GB",
-            rom: "256 GB",
-            maincamera: '48 MP + 50 MP + 8 MP',
-            frontCamera: '32 MP',
-            price: 160000 
+            image: './Assets/Product-7.webp',
+            brand: 'Sony',
+            model: 'PS4 Controller',
+            price: 3700
         },
         {
-            image: "./Assets/Mobile-5.jpg",
-            brand: 'Vivo',
-            model: 'S1',
-            ram: "8 GB",
-            rom: "256 GB",
-            maincamera: '16 MP + 8 MP + 5 MP',
-            frontCamera: '32 MP',
-            price: 32500 
+            image: './Assets/Product-8.webp',
+            brand: 'HP',
+            model: 'Chromebook G8',
+            price: 10700
         },
         {
-            image: "./Assets/Mobile-6.png",
-            brand: 'Apple',
-            model: 'Iphone 14',
-            ram: "6 GB",
-            rom: "128 GB",
-            maincamera: "48 MP",
-            frontCamera: '12 MP',
-            price: 168000 
+            image: './Assets/Product-9.webp',
+            brand: 'Asus',
+            model: '24" LED Monitor',
+            price: 17000
         },
         {
-            image: "./Assets/Mobile-7.png",
+            image: './Assets/Product-10.webp',
+            brand: 'Lenovo',
+            model: 'Wireless Headphone',
+            price: 4400
+        },
+        {
+            image: './Assets/Product-9.webp',
+            brand: 'HP',
+            model: 'RGB Keyboard',
+            price: 4300
+        },
+        {
+            image: './Assets/Product-10.webp',
             brand: 'Samsung',
-            model: 'A04',
-            ram: "3 GB",
-            rom: "32 GB",
-            maincamera: '50 MP',
-            frontCamera: '5 MP',
-            price: 18800
+            model: 'Galaxy Buds 2',
+            price: 2900 
         },
-        {
-            image: "./Assets/Mobile-8.webp",
-            brand: 'Realme',
-            model: 'C67',
-            ram: "8 GB",
-            rom: "128 GB",
-            maincamera: "108 MP + 2 MP",
-            frontCamera: '8 MP',
-            price: 36600 
-        },
-        {
-            image: "./Assets/Mobile-9.webp",
-            brand: 'Realme',
-            model: 'C33',
-            ram: "4 GB",
-            rom: "64 GB",
-            maincamera: '50 MP',
-            frontCamera: '5 MP',
-            price: 30000
-        },
-        {
-            image: "./Assets/Mobile-10.webp",
-            brand: 'Samsung',
-            model: 'Z Flip 4',
-            ram: "8 GB",
-            rom: "512 GB",
-            maincamera: "12 MP + 12 MP",
-            frontCamera: '10 MP',
-            price: 250000 
-        },
-        {
-            image: "./Assets/Mobile-11.webp",
-            brand: 'Oppo',
-            model: 'A18',
-            ram: "4 GB",
-            rom: "128 GB",
-            maincamera: "8 MP + 2 MP",
-            frontCamera: '5 MP',
-            price: 30500 
-        },
-        {
-            image: "./Assets/Mobile-12.webp",
-            brand: 'Tecno',
-            model: 'Spark 20 Pro Plus',
-            ram: "8 GB",
-            rom: "256 GB",
-            maincamera: "108 MP + 2 MP",
-            frontCamera: '32 MP',
-            price: 50000 
-        },
-        {
-            image: "./Assets/Mobile-13.png",
-            brand: 'Oppo',
-            model: 'F1S',
-            ram: "4 GB",
-            rom: "64 GB",
-            maincamera: "13 MP",
-            frontCamera: '16 MP',
-            price: 18000
-        },
-        {
-            image: "./Assets/Mobile-14.webp",
-            brand: 'Xiaomi',
-            model: 'Redmi 13 C',
-            ram: "6 GB",
-            rom: "128 GB",
-            maincamera: "8 MP",
-            frontCamera: '8 MP',
-            price: 26000 
-        },
-        {
-            image: "./Assets/Mobile-15.webp",
-            brand: 'Nokia',
-            model: 'C22',
-            ram: "4 GB",
-            rom: "128 GB",
-            maincamera: "13 MP",
-            frontCamera: '8 MP',
-            price: 24500 
-        },
-        
-    
     ]
 
 
@@ -166,18 +99,10 @@ export default function Products() {
         {phones.map((x:any , i:number)=>{
             return <>
             <div className='border shadow box'>
-                <div>
-                    <img src={x.image} alt="Product Image" />
-                </div>
-                <div>
+                    <img width={'100%'} src={x.image} alt="Product Image" />
                 <p><span className="fw-bolder">Brand:</span> {x.brand}</p>
                       <p><span className="fw-bolder">Model:</span> {x.model}</p>
-                      <p><span className="fw-bolder">Ram:</span> {x.ram}</p>
-                      <p><span className="fw-bolder">Rom:</span> {x.rom}</p>
-                      <p><span className="fw-bolder">Rear Camera:</span> {x.maincamera}</p>
-                      <p><span className="fw-bolder">Front Camera:</span> {x.frontCamera}</p>
                       <p><span className="fw-bolder">Price:</span> Rs. {x.price} </p>
-                </div>
             </div>
             </>
         })}
