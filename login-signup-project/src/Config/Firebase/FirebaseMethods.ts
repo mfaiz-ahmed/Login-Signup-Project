d8e61f792112a779996174baf6b167751b171657
Faiz Ahmed <faizmaqsood668@gmail.com>
2024-06-29 07:22:13 -0700

    authentication completed

--- login-signup-project/src/Config/Firebase/FirebaseMethods.ts
+++ login-signup-project/src/Config/Firebase/FirebaseMethods.ts
@@ -1,20 +1,65 @@
 import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import app from './FirebaseConfig'
+import Swal from 'sweetalert2'
+import 'animate.css';
+
 
 
 
 const auth = getAuth(app)
 
 
+
 export const SignUpUser = (email:string , password:string)=>{
     createUserWithEmailAndPassword(auth , email , password)
     .then(res=>{
         console.log(res , "Success");
-    })
-    .catch(err=>{
-        console.log(err , "Error");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        Swal.fire({
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9L3VNHLIKDobzfP6_1FVe09UAU71t929y4UqT-pvKSD2U0B44iDvCr8FQjvaV2N4PEXs&usqp=CAU",
+            imageHeight: 200,
+            imageAlt: "Tick Image",
+            title: "Signup Successfull!",
+            showClass: {
+              popup: `
+                animate__animated
+                animate__backInUp
+                animate__faster
+              `
+            },
+            hideClass: {
+              popup: `
+                animate__animated
+                animate__backOutDown
+                animate__faster
+              `
+            }
+          });
+        })
+        .catch(err=>{
+            console.log(err , "Error");         
+            Swal.fire({
+                imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSMv3Q1XF6C8X2aZvm-HB1fsngbeoV6CjH90C-KwN6cVolsd9ed",
+                imageHeight: 200,
+                imageAlt: "Cross Image",
+                title: "Signup Unsuccessfull!",
+                showClass: {
+                  popup: `
+                    animate__animated
+                    animate__backInUp
+                    animate__faster
+                  `
+                },
+                hideClass: {
+                  popup: `
+                    animate__animated
+                    animate__backOutDown
+                    animate__faster
+                  `
+                }
+              });                                                                                    
     })
 }
+
 
 
 
