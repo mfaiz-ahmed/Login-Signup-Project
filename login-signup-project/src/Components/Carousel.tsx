a7dd07a48159d64dc624155fbbb308419e67394c
Faiz Ahmed <faizmaqsood668@gmail.com>
2024-06-29 08:00:15 -0700

    project completed

--- login-signup-project/src/Components/Carousel.tsx
+++ login-signup-project/src/Components/Carousel.tsx
@@ -3,6 +3,7 @@ import Carousel1 from './Assets/Carousel-1.png'
 import Carousel2 from './Assets/Carousel-2.png'
 import Carousel3 from './Assets/Carousel-3.png'
 import Carousel4 from './Assets/Carousel-4.png'
+import Carousel5 from './Assets/Carousel-5.png'
 
 export default function Carousel() {
   return (
@@ -20,6 +21,9 @@ export default function Carousel() {
     </div>
     <div className="carousel-item">
       <img src={Carousel4} className="d-block w-100" alt="Carousel-4"/>
+    </div>
+    <div className="carousel-item">
+      <img src={Carousel5} className="d-block w-100" alt="Carousel-5"/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
